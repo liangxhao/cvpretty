7c31651ae1622e8afbcf2ea3b4a1b524478f1723
liangxhao <liangxhao@gmail.com>
2022-09-19 21:22:05 +0800

    init

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Word")
+import tensorflow as tf
+
+indices = [[1, 2, 3]]
+values = [2.0]
+shape = [4, 3, 6]
+
+start = [1, 2, 2]
+size = [1, 1, 1]
+
+
+y = tf.raw_ops.SparseSlice(
+    indices=indices, values=values, shape=shape, start=start, size=size)
+
+print(y)
