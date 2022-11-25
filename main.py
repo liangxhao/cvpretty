c4a757457596e7d348765212aed5fbf7380c6458
liangxhao <liangxhao@gmail.com>
2022-11-25 18:01:52 +0800

    fix

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-import numpy as np
+import torch
+import torch.nn.functional as F
 import tensorflow as tf
+import numpy as np
+np.random.seed(1)
 
 
-x_real = np.random.randn(2, 3)
-x_imag = np.random.randn(2, 3)
-x = x_real + 1j * x_imag
+def _count_unequal_element(data_expected, data_me, rtol, atol):
+    assert data_expected.shape == data_me.shape
+    total_count = len(data_expected.flatten())
+    error = np.abs(data_expected - data_me)
+    greater = np.greater(error, atol + np.abs(data_me) * rtol)
+    loss_count = np.count_nonzero(greater)
+    assert (loss_count / total_count) < rtol, \
+        "\ndata_expected_std:{0}\ndata_me_error:{1}\nloss:{2}". \
+            format(data_expected[greater], data_me[greater], error[greater])
 
-y = tf.cast(x, tf.int32)
-print(y)
-print(x)
 
-isinstance()
+def allclose_nparray(data_expected, data_me, rtol, atol, equal_nan=True):
+    if np.any(np.isnan(data_expected)) or np.any(np.isnan(data_me)):
+        assert np.allclose(data_expected, data_me, rtol, atol, equal_nan=equal_nan)
+    elif not np.allclose(data_expected, data_me, rtol, atol, equal_nan=equal_nan):
+        _count_unequal_element(data_expected, data_me, rtol, atol)
+    else:
+        assert np.array(data_expected).shape == np.array(data_me).shape
 
 
-https://etrans15.huawei.com/valid.aspx?d=GWmaNN/KrsqXmW1aCxoGiCuEZgbC5jZC1o4chA62Ulhlcl4aguw+Kv0bDh+LN7YB9F4QyQfahwEW+dLQRZISEh/payZ/0O/ChC+m0ajpMX3b07+Fuv2NWKpAXJM/IjEj74pu17kYAxym9eUXa82eBKNHUfNw3vneFGkeKYKkSB/YvXlbTHRm8Uqs0Y+jw2QPPrwkj5KxVFOzXem7jTZ9GIAcWUY0GmlJkpsRV1azP04nQUGmIWc8IJhLHZ6BIG9A1G09whNw4a//D2Bq7kc7EYsUp3H0ViEHmM+CSMSAwoZxsh2te5OO4ro8ONEjpt7dRUiVswh6vmbjpBkaXIRyhQ==
+##############################################################################
+# NCHW
+images = np.random.randn(244, 25, 51, 174).astype(np.float32)
+size = [74, 61]
+
+############################ 1 ###############################################
+# tf: align_corners=False, half_pixel_centers=True
+# pt: align_corners=False
+y_tf = tf.raw_ops.ResizeBilinear(images=images.transpose([0, 2, 3, 1]), size=size, align_corners=False, half_pixel_centers=True).numpy().transpose([0, 3, 1, 2])
+y_pt = F.interpolate(torch.from_numpy(images), size, align_corners=False, mode='bilinear').numpy()
+
+allclose_nparray(y_pt, y_tf, 1e-4, 1e-4)
+
+############################ 2 ###############################################
+# tf: align_corners=True, half_pixel_centers=False
+# pt: align_corners=True
+y_tf = tf.raw_ops.ResizeBilinear(images=images.transpose([0, 2, 3, 1]), size=size, align_corners=True, half_pixel_centers=False).numpy().transpose([0, 3, 1, 2])
+y_pt = F.interpolate(torch.from_numpy(images), size, align_corners=True, mode='bilinear').numpy()
+
+allclose_nparray(y_pt, y_tf, 1e-4, 1e-4)
+
+
+############################ 3 ###############################################
+# tf: align_corners=False, half_pixel_centers=False
+# pt: align_corners=False
+y_tf = tf.raw_ops.ResizeBilinear(images=images.transpose([0, 2, 3, 1]), size=size, align_corners=False, half_pixel_centers=False).numpy().transpose([0, 3, 1, 2])
+y_pt = F.interpolate(torch.from_numpy(images), size, align_corners=False, mode='bilinear').numpy()
+
+allclose_nparray(y_pt, y_tf, 1e-4, 1e-4)
